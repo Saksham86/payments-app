91947d8f8d2b344f6662644272b9594176e10484
Saksham86 <bansalsaksham86@gmail.com>
2024-07-08 16:16:11 +0530

    finally working frontedn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,25 +8,17 @@ import { Signin } from "./pages/Signin";
 import { Dashboard } from "./pages/Dashboard";
 import { SendMoney } from "./pages/SendMoney";
 
-
 function App() {
-
   return (
     <>
-    
-
-     <BrowserRouter>
+       <BrowserRouter>
         <Routes>
-          
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/send" element={<SendMoney />} />
         </Routes>
-      </BrowserRouter> 
-
-
-   
+      </BrowserRouter>
     </>
   )
 }
